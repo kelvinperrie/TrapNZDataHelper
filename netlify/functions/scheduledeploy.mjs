7f67f82cfd60fc438e4a5fd333cb95dfe3d0fbf4
Kelvin <kelvin.perrie@tdhb.org.nz>
2025-06-10 08:37:48 +1200

    update scheduled build function to actually (maybe) do a scheduled build

--- netlify/functions/scheduledeploy.mjs
+++ netlify/functions/scheduledeploy.mjs
@@ -7,3 +7,27 @@ export default async (req) => {
 export const config = {
     schedule: "*/5 * * * *"
 }
+
+
+
+import fetch from 'node-fetch'
+import { schedule } from '@netlify/functions'
+
+// This is the 'scheduledeployment' build hook URL
+const BUILD_HOOK =
+  'https://api.netlify.com/build_hooks/6847450fa142580c3cbd53df'
+
+// schedule this function
+const handler = schedule("*/5 * * * *", async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then(response => {
+    console.log('Build hook response:', response)
+  })
+
+  return {
+    statusCode: 200
+  }
+})
+
+export { handler }

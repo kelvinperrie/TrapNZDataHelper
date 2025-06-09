b5cee97265ca61c46fd1a881e69abb298310c2ce
Kelvin <kelvin.perrie@tdhb.org.nz>
2025-06-10 09:19:22 +1200

    set the schedule to run after the github action has pulled in the updated data files

--- netlify/functions/scheduledeploy.mjs
+++ netlify/functions/scheduledeploy.mjs
@@ -6,7 +6,7 @@ const BUILD_HOOK =
   'https://api.netlify.com/build_hooks/6847450fa142580c3cbd53df'
 
 // schedule this function
-const handler = schedule("*/5 * * * *", async () => {
+const handler = schedule("30 18 * * *", async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then(response => {
